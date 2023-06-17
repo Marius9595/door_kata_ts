30e3c4bab995d396470b874e24347412f277391a
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-17 01:02:42 +0100

    :tada: Starting point

---
+++ src/core/door.ts
@@ -0,0 +1,5 @@
+export class Door {
+	processEvents(events: string): string {
+		return undefined;
+	}
+}
---
+++ src/tests/door.test.ts
@@ -0,0 +1,21 @@
+
+/*
+	CASES:
+	 - door is closed at the beginning
+	 - door will star open when the button is pressed
+	 - door needs 5 seconds to open
+	 - door stays open after 5 seconds
+	 - door will close when the button is pressed again
+	 - door needs 5 seconds to close
+	 - door stays closed after 5 seconds
+	 - door will be paused when button is pressed during opening
+	 - door will be paused when button is pressed during closing
+	 - door will continue when button is pressed again
+	 - door will change his direction when detecting an obstacle
+*/
+
+describe('door', () => {
+	it('should start', function () {
+
+	});
+});
