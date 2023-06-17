fee969473a282451e16f171e4bd2f34d88c0cea1
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-17 02:22:05 +0100

    🟢 door should be open after 5 seconds since button was pressed

--- src/core/door.ts
+++ src/core/door.ts
@@ -1,13 +1,52 @@
-export class Door {
+interface DoorState {
+	processEvents(events: string): string;
+}
+
+class Closed implements DoorState {
+	constructor(private door: Door) {}
 	processEvents(events: string): string {
-		return events
-			.split('')
-			.map((event) => {
-				if (event === 'P') {
-					return '1';
+		if (!events.includes('P')) {
+			return events.replace('.', '0');
+		}
+
+		const indexOfButtonWasPressed = events.indexOf('P');
+		const procceedEvents = events.slice(0, indexOfButtonWasPressed).replace('.', '0');
+		const eventsToProcess = events.slice(indexOfButtonWasPressed);
+		this.door.changeState(new Opening(this.door));
+
+		return this.door.processEvents(procceedEvents + eventsToProcess);
+	}
+}
+
+class Opening implements DoorState {
+	constructor(private door: Door) {}
+
+	processEvents(events: string): string {
+		const indexOfButtonWasPressed = events.indexOf('P');
+		const procceedEvents = events.slice(0, indexOfButtonWasPressed).replace('.', '0');
+		const eventsToProcess = events.slice(indexOfButtonWasPressed);
+
+		let eventsInProcessing = procceedEvents;
+		if (!eventsToProcess.substring(1).includes('P')) {
+			if (eventsToProcess.length > 5) {
+				return (eventsInProcessing += '12345' + eventsToProcess.slice(5).replace('.', '5'));
+			} else {
+				for (let i = 0; i < eventsToProcess.length; i++) {
+					eventsInProcessing += i + 1;
 				}
-				return '0';
-			})
-			.join('');
+				return eventsInProcessing;
+			}
+		}
+		return ""
+	}
+}
+
+export class Door {
+	private state: DoorState = new Closed(this);
+	processEvents(events: string): string {
+		return this.state.processEvents(events);
+	}
+	changeState(state: DoorState) {
+		this.state = state;
 	}
 }
