40af526f33993abaa8308b84dbe2eb329762721e
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-21 20:19:21 +0100

    🟢 door should be open after 5 seconds if button is not pressed or obstacule is not detected

--- src/core/door_state/Opening.ts
+++ src/core/door_state/Opening.ts
@@ -4,6 +4,13 @@ import { Door } from '../door';
 export class Opening implements DoorState {
 	constructor(private door: Door) {}
 	processEvents(events: string): string {
-		return '1';
+		const eventsToProcess = events.split('');
+		let index = 1;
+		let eventsProcessed = '1';
+		while (eventsToProcess[index] === '.' && index < eventsToProcess.length) {
+			eventsProcessed += (index + 1).toString() ;
+			index++;
+		}
+    return eventsProcessed;
 	}
 }
