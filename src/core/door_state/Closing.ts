1b03c556616766cb5cccaf607366d185e3653927
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-21 23:04:46 +0100

    🟢 door should be closed after 5 seconds if button is not pressed or obstacule is not detected

--- src/core/door_state/Closing.ts
+++ src/core/door_state/Closing.ts
@@ -1,9 +1,27 @@
 import { DoorState } from './DoorState';
 import { Door } from '../door';
+import { Open } from "./Open";
 
 export class Closing implements DoorState {
 	constructor(private door: Door) {}
 	processEvents(events: string): string {
-		return '4';
+    const eventsToProcess = events.split('');
+    let index = 1;
+    let eventsProcessed = '4';
+    while (
+      eventsToProcess[index] === '.' &&
+      index < eventsToProcess.length &&
+      !eventsProcessed.includes('0')
+      ) {
+      eventsProcessed += (4- index).toString();
+      index++;
+    }
+    const isClosed = '0';
+    if (eventsProcessed.includes(isClosed) && index < eventsToProcess.length) {
+      this.door.changeState(new Open(this.door));
+      const restOfEvents = events.substring(index);
+      return eventsProcessed + this.door.processEvents(restOfEvents);
+    }
+    return eventsProcessed;
 	}
 }
