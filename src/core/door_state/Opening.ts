c21169f5096459ac7f78c89df0ea99e70e097371
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-21 23:10:31 +0100

    :recycle: extracted explaining variable

--- src/core/door_state/Opening.ts
+++ src/core/door_state/Opening.ts
@@ -6,18 +6,17 @@ export class Opening implements DoorState {
 	constructor(private door: Door) {}
 	processEvents(events: string): string {
 		const eventsToProcess = events.split('');
+		const open = '5';
 		let index = 1;
-		let eventsProcessed = '1';
-		while (
-			eventsToProcess[index] === '.' &&
-			index < eventsToProcess.length &&
-      !eventsProcessed.includes('5')
-		) {
-			eventsProcessed += (index + 1).toString();
+		const firstOpeningPosition = '1';
+		let eventsProcessed = firstOpeningPosition;
+		while (eventsToProcess[index] === '.' && index < eventsToProcess.length && !eventsProcessed.includes(open)) {
+			const nextOpeningPosition = (index + 1).toString();
+			eventsProcessed += nextOpeningPosition;
 			index++;
 		}
-		const isOpen = '5';
-		if (eventsProcessed.includes(isOpen) && index < eventsToProcess.length) {
+
+		if (eventsProcessed.includes(open) && index < eventsToProcess.length) {
 			this.door.changeState(new Open(this.door));
 			const restOfEvents = events.substring(index);
 			return eventsProcessed + this.door.processEvents(restOfEvents);
