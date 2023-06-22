d4d8a40158562c9a1de16b9d18b14f489e8a5321
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-22 21:05:58 +0100

    :recycle: extracted explaining variable

--- src/core/door_state/Open.ts
+++ src/core/door_state/Open.ts
@@ -1,15 +1,16 @@
-import { DoorState } from "./DoorState";
-import { Door } from "../door";
-import { Closing } from "./Closing";
+import { DoorState } from './DoorState';
+import { Door } from '../door';
+import { Closing } from './Closing';
 
 export class Open implements DoorState {
 	constructor(private door: Door) {}
 	processEvents(events: string): string {
-		let first = 0;
+		const first = 0;
 		let eventsProcessed = '';
 		const eventsToProcess = events.split('');
 
-		if(eventsToProcess[first] === '.') {
+		const isNoEvent = eventsToProcess[first] === '.';
+		if (isNoEvent) {
 			const open = '5';
 			eventsProcessed += open;
 			return eventsProcessed + this.door.processEvents(events.substring(first + 1));
