62fdd87e80371c304438b7c064d05ec56f67868f
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-06-22 21:24:29 +0100

    :recycle:  extracted explaining variables

--- src/core/door_state/Closing.ts
+++ src/core/door_state/Closing.ts
@@ -1,7 +1,5 @@
-import { DoorState } from './DoorState';
-import { Door } from '../door';
-import { Open } from './Open';
-import { PausedOpening } from "./PausedOpening";
+import { DoorState } from "./DoorState";
+import { Door } from "../door";
 import { PausedClosing } from "./PausedClosing";
 import { Closed } from "./Closed";
 import { Opening } from "./Opening";
@@ -10,45 +8,54 @@ export class Closing implements DoorState {
 	constructor(private door: Door) {}
 	processEvents(events: string): string {
 		const eventsToProcess = events.split('');
-		const open = '5';
-		const closed = '0';
-
 		let index = 1;
 		let eventsProcessed;
-		if (eventsToProcess[0] !== closed && eventsToProcess[0] !== 'P') {
+		if (eventsToProcess[0] !== '0' && eventsToProcess[0] !== 'P') {
 			const currentOpeningPosition = eventsToProcess[0];
 			eventsProcessed = (parseInt(currentOpeningPosition) - 1).toString();
 		} else {
 			const firstOpeningPosition = '4';
 			eventsProcessed = firstOpeningPosition;
 		}
-		while (eventsToProcess[index] === '.' && index < eventsToProcess.length && !eventsProcessed.includes(open)) {
+		while (eventsToProcess[index] === '.' && index < eventsToProcess.length && !eventsProcessed.includes('5')) {
 			const nextOpeningPosition = (4 - index).toString();
 			eventsProcessed += nextOpeningPosition;
 			index++;
 		}
 
-		const obstacleDetected = 'O';
-		if (eventsToProcess.includes(obstacleDetected) && index < eventsToProcess.length && !eventsProcessed.includes(closed)) {
-			this.door.changeState(new Opening(this.door));
-			const restOfEvents = events.substring(index);
-			const lastEvent = eventsProcessed.split('')[eventsProcessed.length - 1];
-			return eventsProcessed + this.door.processEvents(lastEvent.toString() + restOfEvents.substring(1));
+
+		if(index < eventsToProcess.length){
+
+			const obstaculeDetectedEvent = eventsToProcess.includes('O');
+			const isClosedEvent = eventsProcessed.includes('0');
+			if (
+				obstaculeDetectedEvent &&
+				!isClosedEvent
+			) {
+				this.door.changeState(new Opening(this.door));
+				const restOfEvents = events.substring(index);
+				const lastEvent = eventsProcessed.split('')[eventsProcessed.length - 1];
+				return eventsProcessed + this.door.processEvents(lastEvent.toString() + restOfEvents.substring(1));
+			}
+			const buttonWasPressedEvent = eventsToProcess.includes('P');
+			if (
+				buttonWasPressedEvent &&
+				!isClosedEvent
+			) {
+				this.door.changeState(new PausedClosing(this.door));
+				const restOfEvents = events.substring(index);
+				const lastEvent = eventsProcessed.split('')[eventsProcessed.length - 1];
+				return eventsProcessed + this.door.processEvents(lastEvent + restOfEvents.substring(1));
+			}
+
+			if (isClosedEvent) {
+				this.door.changeState(new Closed(this.door));
+				const restOfEvents = events.substring(index);
+				const lastEvent = eventsProcessed.split('')[eventsProcessed.length - 1];
+				return eventsProcessed + this.door.processEvents(lastEvent + restOfEvents);
+			}
 		}
 
-		const buttonPressed = 'P';
-		if (eventsToProcess.includes(buttonPressed) && index < eventsToProcess.length && !eventsProcessed.includes(closed)) {
-			this.door.changeState(new PausedClosing(this.door));
-			const restOfEvents = events.substring(index);
-			const lastEvent = eventsProcessed.split('')[eventsProcessed.length - 1];
-			return eventsProcessed + this.door.processEvents(lastEvent + restOfEvents.substring(1));
-		}
-
-		if (eventsProcessed.includes(closed) && index < eventsToProcess.length) {
-			this.door.changeState(new Closed(this.door));
-			const restOfEvents = events.substring(index);
-			return eventsProcessed + this.door.processEvents(restOfEvents);
-		}
 		return eventsProcessed;
 	}
 }
